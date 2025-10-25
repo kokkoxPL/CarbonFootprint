305858acb6e5079a1764c52c2d031f6c659b7f8d
kokkoxPL <adriangamracy@gmail.com>
2025-10-25 04:56:55 +0200

    removed problematic line

--- app/src/main/java/com/kokkoxpl/carbonfootprint/fragments/AboutFragment.java
+++ app/src/main/java/com/kokkoxpl/carbonfootprint/fragments/AboutFragment.java
@@ -3,15 +3,14 @@ package com.kokkoxpl.carbonfootprint.fragments;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.View;
-import android.widget.ImageButton;
 
 import com.kokkoxpl.carbonfootprint.R;
 import com.kokkoxpl.carbonfootprint.adapters.GroupListAdapter;
@@ -42,6 +41,5 @@ public class AboutFragment extends Fragment {
 
         recyclerView.setAdapter(groupListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        recyclerView.setHasFixedSize(true);
     }
 }
